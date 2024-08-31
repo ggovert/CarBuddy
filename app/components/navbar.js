1f758dcae615ee2a1a105e91e6b2ce55fdd5ec4e
Giovanni <giovannigovert@gmail.com>
2024-08-31 01:33:04 -0500

    Update navbar.js

--- app/components/navbar.js
+++ app/components/navbar.js
@@ -1,22 +1,35 @@
-import { AppBar, Toolbar, Button, Box, 
-  ThemeProvider,InputAdornment
-,DialogTitle, Dialog, DialogActions, DialogContent, DialogContentText, TextField } from "@mui/material";
+import {
+  AppBar,
+  Toolbar,
+  Button,
+  Box,
+  ThemeProvider,
+  DialogTitle,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogContentText,
+  TextField,
+} from "@mui/material";
 import Theme from "./theme";
 import { useRouter } from "next/navigation";
-import React, { useEffect } from "react";
-import {  } from "react"; // Added import
+import React, { useState, useEffect } from "react";
+import db from "@/firebase"; // Adjust the import path as necessary
+import { collection, addDoc } from "firebase/firestore";
 
 const Navbar = () => {
   const router = useRouter();
-  const [open, setOpen] = React.useState(false);
+  const [open, setOpen] = useState(false);
+  const [email, setEmail] = useState("");
+  const [status, setStatus] = useState({ type: "", message: "" });
+
   const handleRedirectContactUs = () => {
     router.push("/contact-us");
   };
+
   const handleRedirectFaq = () => {
     router.push("/faq");
   };
-
-  
 
   const handleClickOpen = () => {
     setOpen(true);
@@ -24,16 +37,43 @@ const Navbar = () => {
 
   const handleClose = () => {
     setOpen(false);
+    setStatus({ type: "", message: "" });
   };
 
-  // _____________________________ Nav Bar UI _____________________________
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setStatus({ type: "", message: "" });
+
+    if (!email) {
+      setStatus({ type: "error", message: "Please enter your email." });
+      return;
+    }
+
+    try {
+      const docRef = await addDoc(collection(db, "waitlist"), {
+        email: email,
+        timestamp: new Date(),
+      });
+      setStatus({
+        type: "success",
+        message: "You have been added to the waitlist!",
+      });
+      setEmail("");
+      setTimeout(() => {
+        handleClose();
+      }, 2000); // Close the dialog after 2 seconds
+    } catch (error) {
+      setStatus({
+        type: "error",
+        message: "An error occurred. Please try again.",
+      });
+      console.error("Error adding document: ", error);
+    }
+  };
 
   return (
     <ThemeProvider theme={Theme}>
-      <AppBar
-        position="static"
-        sx={{ backgroundColor: 'black' }}
-      >
+      <AppBar position="static" sx={{ backgroundColor: "black" }}>
         <Toolbar>
           <Box
             display="flex"
@@ -42,55 +82,31 @@ const Navbar = () => {
             width="100%"
           >
             <Box>
-              <a href="/" >
+              <a href="/">
                 <img
                   src="/logo.png"
-                  href="/"
                   width={70}
                   height={31}
                   alt="logo car buddy"
-                  
                 />
               </a>
             </Box>
-            <Box >
-              {/* <Button
-                variant="outlined"
-                sx={{
-                  borderRadius: "16px",
-                  boxShadow: "none",
-                  fontFamily: "Poppins",
-                  backgroundColor: Theme.palette.primary.dark,
-                  "&:hover": {
-                    bgcolor: Theme.palette.text.darker,
-                  },
-                }}
-                onClick={handleRedirectChatbot}
-              >
-                Chat with AI
-              </Button> */}
+            <Box>
               <Button
                 variant="text"
                 sx={{
                   boxShadow: "none",
                   fontFamily: "Montserrat",
                   color: Theme.palette.text.green,
-                  fontSize: {
-                    xs: "10px", 
-                    sm: "12px", 
-                    md: "12px", 
-                    lg: "18px", 
-                  },
+                  fontSize: { xs: "10px", sm: "12px", md: "12px", lg: "18px" },
                   padding: {
-                    xs: '4px 5px 4px 20px', 
-                    sm: '4px 5px 4px 20px', 
-                    md: '4px 30px 4px 30px', 
-                    lg: '4px 30px 4px 30px', 
+                    xs: "4px 5px 4px 20px",
+                    sm: "4px 5px 4px 20px",
+                    md: "4px 30px 4px 30px",
+                    lg: "4px 30px 4px 30px",
                   },
-                  textDecoration: "none", 
-                  "&:hover": {
-                    textDecoration: "underline", 
-                  },
+                  textDecoration: "none",
+                  "&:hover": { textDecoration: "underline" },
                 }}
                 onClick={handleRedirectFaq}
               >
@@ -101,114 +117,104 @@ const Navbar = () => {
                 sx={{
                   boxShadow: "none",
                   fontFamily: "Montserrat",
-                  fontSize: {
-                    xs: "10px", 
-                    sm: "12px", 
-                    md: "12px", 
-                    lg: "18px", 
-                  },
+                  fontSize: { xs: "10px", sm: "12px", md: "12px", lg: "18px" },
                   padding: {
-                    xs: '4px 20px 4px 0px', 
-                    sm: '4px 20px, 4px 0px', 
-                    md: '8px 30px 4px 0px', 
-                    lg: '8px 30px 4px 0px', 
+                    xs: "4px 20px 4px 0px",
+                    sm: "4px 20px, 4px 0px",
+                    md: "8px 30px 4px 0px",
+                    lg: "8px 30px 4px 0px",
                   },
                   color: Theme.palette.text.green,
                   textDecoration: "none",
-                  "&:hover": {
-                    textDecoration: "underline", 
-                  },
+                  "&:hover": { textDecoration: "underline" },
                 }}
                 onClick={handleRedirectContactUs}
               >
                 Contact US
               </Button>
-
-
-              <React.Fragment>
-                <Button
-                  type="submit"
-                  variant="contained"
-                  color="primary"
-                  sx={{
-                    background: Theme.palette.primary.green,
-                    borderRadius: 6,
-                    padding: {
-                      xs: '4px 20px', 
-                      sm: '4px 20px', 
-                      md: '8px 40px', 
-                      lg: '8px 40px', 
-                    },
-                    color: 'black',
-                    textTransform: 'none',
-                  }}
-                  onClick={handleClickOpen}
-                >
-                  JOIN NOW
-                </Button>
-                <Dialog
-                  open={open}
-                  onClose={handleClose}
-                  PaperProps={{
-                    sx: {
-                      background: `linear-gradient(to bottom, black, #005852)`, 
-                      borderRadius: 6, // Add borderRadius
-                    },
-                    component: 'form',
-                    onSubmit: (event) => {
-                      event.preventDefault();
-                      const formData = new FormData(event.currentTarget);
-                      const formJson = Object.fromEntries(formData.entries());
-                      const email = formJson.email;
-                      console.log(email);
-                      handleClose();
-                    },
-                  }}
-                >
-                  <DialogTitle sx={{
-            color: Theme.palette.primary.green, // Change title color to green
-          }}
-                ></DialogTitle>
-                  <DialogContent sx={{
-                        color: 'white', // Change title color to green
-                      }}>
-                    <DialogContentText sx={{
-            color: Theme.palette.primary.green, // Change title color to green
-          }} >
-                      Get in!
-                    </DialogContentText >
-                    <TextField
-                      autoFocus
-                      required
-                      margin="dense"
-                      id="name"
-                      name="email"
-                      label="Email Address"
-                      type="email"
-                      fullWidth
-                      variant="standard"
-                      InputLabelProps={{
-                        sx: { color: 'white' }, // Change label color to white
-                      }}
-                      InputProps={{
-                        sx: { color: 'white' }, // Change input text color to white
-                      }}
-                      
-                      
-                    />
-                  </DialogContent>
-                  <DialogActions>
-                    <Button variant="text" onClick={handleClose} sx={{
-                    color: Theme.palette.text.green}}>Cancel</Button>
-                    <Button variant="text" type="submit" sx={{
-                    color: Theme.palette.text.green,}}>Subscribe</Button>
-                  </DialogActions>
-                </Dialog>
-              </React.Fragment>
+              <Button
+                variant="contained"
+                color="primary"
+                sx={{
+                  background: Theme.palette.primary.green,
+                  borderRadius: 6,
+                  padding: {
+                    xs: "4px 20px",
+                    sm: "4px 20px",
+                    md: "8px 40px",
+                    lg: "8px 40px",
+                  },
+                  color: "black",
+                  textTransform: "none",
+                }}
+                onClick={handleClickOpen}
+              >
+                JOIN NOW
+              </Button>
             </Box>
           </Box>
         </Toolbar>
       </AppBar>
+
+      <Dialog
+        open={open}
+        onClose={handleClose}
+        PaperProps={{
+          sx: {
+            background: `linear-gradient(to bottom, black, #005852)`,
+            borderRadius: 6,
+          },
+        }}
+      >
+        <DialogTitle sx={{ color: Theme.palette.primary.green }}>
+          Join the Waitlist
+        </DialogTitle>
+        <DialogContent>
+          <DialogContentText sx={{ color: Theme.palette.primary.green }}>
+            Get in!
+          </DialogContentText>
+          <TextField
+            autoFocus
+            required
+            margin="dense"
+            id="email"
+            name="email"
+            label="Email Address"
+            type="email"
+            fullWidth
+            variant="standard"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            InputLabelProps={{ sx: { color: "white" } }}
+            InputProps={{ sx: { color: "white" } }}
+          />
+          {status.message && (
+            <DialogContentText
+              sx={{
+                color:
+                  status.type === "error" ? "red" : Theme.palette.primary.green,
+                mt: 2,
+              }}
+            >
+              {status.message}
+            </DialogContentText>
+          )}
+        </DialogContent>
+        <DialogActions>
+          <Button
+            onClick={handleClose}
+            sx={{ color: Theme.palette.text.green }}
+          >
+            Cancel
+          </Button>
+          <Button
+            onClick={handleSubmit}
+            sx={{ color: Theme.palette.text.green }}
+          >
+            Subscribe
+          </Button>
+        </DialogActions>
+      </Dialog>
     </ThemeProvider>
   );
 };
